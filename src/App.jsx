4a1039dcc0c9b28646009098063d6e64b2b6717c
riwu <me@wangriwu.com>
2018-09-26 20:37:54 +0800

    add basic setup, config and packages

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { hot } from 'react-hot-loader';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import Routes from './routes';
+
+const middleware = [thunk];
+const config = {
+  key: 'root',
+  storage,
+};
+
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(
+  persistReducer(config, reducer),
+  composeEnhancers(applyMiddleware(...middleware)),
+);
+const persistor = persistStore(store);
+
+const App = () => (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <Routes />
+    </PersistGate>
+  </Provider>
+);
+
+export default hot(module)(App);
