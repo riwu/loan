7f100c3bd33c973c521bded0b55c2b0c0d575f5e
riwu <me@wangriwu.com>
2018-09-27 22:25:37 +0800

    add basic component test

--- src/components/__test__/LoanTermSelection.test.jsx
+++ src/components/__test__/LoanTermSelection.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/App';
+import LoanTermSelection from '../LoanTermSelection';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<LoanTermSelection value={{ number: 1, unit: 'week' }} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
